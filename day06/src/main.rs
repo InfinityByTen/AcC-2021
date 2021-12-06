179d583a83e75764906200d7194c10926b8aadab
Aseem Dua <aseem.dua@here.com>
2021-12-06 13:03:51 +0100

    Add proper solution and prettify

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,7 +1,42 @@
 use std::{collections::HashMap, fs};
 
-fn solve_pre_comp(input: &Vec<i32>) {
-    // obtained by running simulation for each of the values using solving_stupid method.
+fn solve_proper(input: &Vec<i32>, days: u32) {
+    let mut counters = HashMap::<i32, usize>::new();
+    input.iter().for_each(|state| {
+        counters.entry(*state).and_modify(|e| *e += 1).or_insert(1);
+    });
+    (0..=8).for_each(|state| {
+        counters.entry(state).or_insert(0);
+    });
+    let mut working = counters.clone();
+    (0..days).for_each(|_day| {
+        (0..8).for_each(|state| {
+            if state == 0 {
+                // println!("Processing 0 {:?}", counters);
+                working.entry(8).and_modify(|e| *e = counters[&0]);
+                working.entry(6).and_modify(|e| *e = counters[&0]);
+                working.entry(0).and_modify(|e| *e = counters[&1]);
+            } else if state == 6 {
+                working
+                    .entry(state)
+                    .and_modify(|e| *e += counters[&(state + 1)]);
+            } else {
+                {
+                    working
+                        .entry(state)
+                        .and_modify(|e| *e = counters[&(state + 1)]);
+                }
+            }
+        });
+        counters = working.clone();
+    });
+    println!("{:?}", counters.values().sum::<usize>());
+}
+
+// Solution for part 2 using precomputations for 256 days
+fn solve_pre_comp_256(input: &Vec<i32>) {
+    // obtained by running simulation for each of the starting values using solving_stupid method.
+    // Runs under a few mins with 32 GB memory.
     let hack_map = HashMap::<i32, usize>::from([
         (1, 6206821033),
         (2, 5617089148),
@@ -13,9 +48,9 @@ fn solve_pre_comp(input: &Vec<i32>) {
     println!("{:?}", count);
 }
 
-fn solve_stupid(input: &Vec<i32>) {
+fn solve_stupid(input: &Vec<i32>, days: u32) {
     let mut working = input.clone();
-    (0..80).for_each(|_day| {
+    (0..days).for_each(|_day| {
         let mut spawned = 0;
         working.iter_mut().for_each(|e| {
             *e -= 1;
@@ -30,8 +65,9 @@ fn solve_stupid(input: &Vec<i32>) {
 }
 
 fn main() {
-    let buf = fs::read_to_string("./input.txt").unwrap();
+    let buf = fs::read_to_string("./test.txt").unwrap();
     let input = buf.split(',').map(|x| x.parse::<i32>().unwrap()).collect();
-    solve_stupid(&input);
-    solve_pre_comp(&input);
+    solve_stupid(&input, 80);
+    solve_pre_comp_256(&input);
+    solve_proper(&input, 256);
 }
