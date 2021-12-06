82323e4ae7e0b8f0040fbbad6183b07496b7c678
Aseem Dua <aseem.dua@here.com>
2021-12-06 14:20:14 +0100

    Day 6: Remove unnecessary hashmap

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,36 +1,29 @@
 use std::{collections::HashMap, fs};
 
 fn solve_proper(input: &Vec<i32>, days: u32) {
-    let mut counters = HashMap::<i32, usize>::new();
-    input.iter().for_each(|state| {
-        counters.entry(*state).and_modify(|e| *e += 1).or_insert(1);
-    });
-    (0..=8).for_each(|state| {
-        counters.entry(state).or_insert(0);
-    });
+    let mut counters = vec![0; 9];
+    input
+        .iter()
+        .for_each(|state| counters[*state as usize] += 1);
+
     let mut working = counters.clone();
     (0..days).for_each(|_day| {
         (0..8).for_each(|state| {
             if state == 0 {
-                // println!("Processing 0 {:?}", counters);
-                working.entry(8).and_modify(|e| *e = counters[&0]);
-                working.entry(6).and_modify(|e| *e = counters[&0]);
-                working.entry(0).and_modify(|e| *e = counters[&1]);
+                working[8] = counters[0];
+                working[6] = counters[0];
+                working[0] = counters[1];
             } else if state == 6 {
-                working
-                    .entry(state)
-                    .and_modify(|e| *e += counters[&(state + 1)]);
+                working[state] += counters[(state + 1)];
             } else {
                 {
-                    working
-                        .entry(state)
-                        .and_modify(|e| *e = counters[&(state + 1)]);
+                    working[state] = counters[(state + 1)];
                 }
             }
         });
         counters = working.clone();
     });
-    println!("{:?}", counters.values().sum::<usize>());
+    println!("{:?}", counters.iter().sum::<usize>());
 }
 
 // Solution for part 2 using precomputations for 256 days
