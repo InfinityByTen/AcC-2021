ffcaed2de1b0953206852d1d7002813309aa3d4c
Aseem Dua <aseem.dua@here.com>
2021-12-14 11:40:47 +0100

    Day 14: Part2, with a proper combined solution

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,8 +1,56 @@
+use core::hash::Hash;
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs;
 
-fn solve_1(input: &(&str, &str)) {
+fn freq_mapper<T: Eq + Hash>(freq: &mut HashMap<T, usize>, key: T, val: usize) {
+    freq.entry(key).and_modify(|c| *c += val).or_insert(val);
+}
+
+fn solve(input: &str, rules: &HashMap<String, char>, max_steps: u32) {
+    let template = input.to_string();
+    let mut pattern_counts = HashMap::new();
+    let temp_chars = template.chars().collect::<Vec<char>>();
+    temp_chars.windows(2).for_each(|a| {
+        freq_mapper(&mut pattern_counts, a.iter().collect::<String>(), 1);
+    });
+
+    (0..max_steps).for_each(|_| {
+        let mut dummy = HashMap::new();
+        pattern_counts.iter().for_each(|pattern| {
+            let evolution = rules[pattern.0.as_str()];
+            let a = [pattern.0.chars().nth(0).unwrap(), evolution]
+                .iter()
+                .collect::<String>();
+            let b = [evolution, pattern.0.chars().nth(1).unwrap()]
+                .iter()
+                .collect::<String>();
+            freq_mapper(&mut dummy, a, *pattern.1);
+            freq_mapper(&mut dummy, b, *pattern.1);
+        });
+        pattern_counts = dummy;
+    });
+
+    let mut char_counts = HashMap::new();
+    pattern_counts.iter().for_each(|pc| {
+        pc.0.chars()
+            .for_each(|c| freq_mapper(&mut char_counts, c, *pc.1))
+    });
+    char_counts
+        .entry(template.chars().nth(0).unwrap())
+        .and_modify(|f| *f += 1);
+    char_counts
+        .entry(template.chars().nth(template.len() - 1).unwrap())
+        .and_modify(|f| *f += 1);
+    println!(
+        "{:?}",
+        (char_counts.values().max().unwrap() - char_counts.values().min().unwrap()) / 2
+    );
+}
+
+fn main() {
+    let buf = fs::read_to_string("./input.txt").unwrap();
+    let input = buf.split("\n\n").collect_tuple::<(&str, &str)>().unwrap();
     let rules = input
         .1
         .split('\n')
@@ -11,41 +59,6 @@ fn solve_1(input: &(&str, &str)) {
             (rule.0.to_string(), rule.1.chars().nth(0).unwrap())
         })
         .collect::<HashMap<String, char>>();
-    println!("{:?}", rules);
-
-    let mut bam = input.0.to_string();
-
-    (0..10).for_each(|_| {
-        let template = bam.chars().collect::<Vec<char>>();
-        // println!("{:?}", bam);
-
-        let elements = template
-            .windows(2)
-            .map(|a| a.iter().collect::<String>())
-            .collect::<Vec<String>>();
-
-        let mut dummy = Vec::new();
-        (0..template.len() - 1).for_each(|pos| {
-            dummy.push(template[pos]);
-            dummy.push(*rules.get(&elements[pos]).unwrap());
-        });
-        dummy.push(template[template.len() - 1]);
-        bam = dummy.iter().collect();
-    });
-
-    let mut freq = HashMap::new();
-    bam.chars().for_each(|c| {
-        let _ = freq.entry(c).and_modify(|f| *f += 1).or_insert(1);
-    });
-    println!("{:?}", freq);
-    println!(
-        "{:?}",
-        freq.values().max().unwrap() - freq.values().min().unwrap()
-    );
-}
-
-fn main() {
-    let buf = fs::read_to_string("./test.txt").unwrap();
-    let input = buf.split("\n\n").collect_tuple::<(&str, &str)>().unwrap();
-    solve_1(&input);
+    solve(input.0, &rules, 10);
+    solve(input.0, &rules, 40);
 }
