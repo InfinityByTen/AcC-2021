ae65bf1b554a0ace5727afd654797162d0286388
Aseem Dua <aseem.dua@here.com>
2021-12-16 20:44:58 +0100

    Day 16: attempt at part2
    Works on examples, not on input

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,23 +1,41 @@
 use std::fs;
 use to_binary::BinaryString;
 
-fn parse_literal(cursor: &mut dyn Iterator<Item = &char>) -> usize {
+fn parse_literal(cursor: &mut dyn Iterator<Item = &char>) -> (usize, usize) {
     let mut count = 6;
+    let res: usize;
     loop {
         let val = (0..5).map(|_| cursor.next().unwrap()).collect::<String>();
-        // println!("{:?}", val);
         count += 5;
         if &val[0..1] == "0" {
+            res = usize::from_str_radix(&val, 2).unwrap();
             break;
         }
     }
-    count
+    (res, count)
 }
 
+fn operate<'a>(operands: &Vec<usize>, id: &str) -> usize {
+    let id_val = usize::from_str_radix(id, 2).unwrap();
+
+    match id_val {
+        0 => operands.iter().sum(),
+        1 => operands.iter().product(),
+        2 => operands.iter().cloned().min().unwrap(),
+        3 => operands.iter().cloned().max().unwrap(),
+        // 4=> ,
+        5 => ((operands[0] > operands[1]) as usize),
+        6 => ((operands[0] < operands[1]) as usize),
+        7 => ((operands[0] == operands[1]) as usize),
+        _ => unreachable!(),
+    }
+}
+
+#[allow(unused_assignments)]
 fn parse_packet(
     cursor: &mut dyn Iterator<Item = &char>,
     start_tracking_used: bool,
-) -> (usize, usize) {
+) -> (usize, usize, usize) {
     let version = (0..3).map(|_| cursor.next().unwrap()).collect::<String>();
     let type_id = (0..3).map(|_| cursor.next().unwrap()).collect::<String>();
     // println!("{:?}", (&version, &type_id));
@@ -25,16 +43,19 @@ fn parse_packet(
     let mut ver = usize::from_str_radix(&version, 2).unwrap();
 
     let mut used = 0;
+    let mut out = 0;
     if start_tracking_used {
         used += 6;
     }
     match type_id.as_str() {
         "100" => {
             // println!("Literal");
-            return (ver, parse_literal(cursor));
+            let res = parse_literal(cursor);
+            // println!("{:?}", res.0);
+            return (ver, res.0, res.1);
         }
-        _ => {
-            // println!("Operator");
+        op => {
+            // println!("Operator {:?}", usize::from_str_radix(op, 2).unwrap());
             let length_id = cursor.next().unwrap();
             if start_tracking_used {
                 used += 1;
@@ -48,16 +69,21 @@ fn parse_packet(
                     }
                     let mut limit = usize::from_str_radix(&length, 2).unwrap();
                     // println!("Has total length {:?}", limit);
+                    let mut operands = Vec::new();
                     while limit > 0 {
                         // println!("Limit: {:?}", limit);
                         // println!("Used before starting {:?}", used);
                         let res = parse_packet(cursor, true);
                         // println!("used up {:?} bits", res.1);
-                        limit -= res.1;
+                        limit -= res.2;
                         ver += res.0;
-                        used += res.1;
+                        used += res.2;
                         // println!("Used before after one Operator {:?}", used);
+                        operands.push(res.1);
                     }
+                    // println!("{:?}", (&operands, usize::from_str_radix(op, 2).unwrap()));
+                    out = operate(&operands, op);
+                    // println!("{:?}", out);
                 }
                 _ => {
                     let count = (0..11).map(|_| cursor.next().unwrap()).collect::<String>();
@@ -67,25 +93,30 @@ fn parse_packet(
                     let limit = usize::from_str_radix(&count, 2).unwrap();
                     // println!("Has packet count {:?}", limit);
                     // println!("Used before starting {:?}", used);
-                    (0..limit).for_each(|_| {
-                        let res = parse_packet(cursor, start_tracking_used);
-                        // println!("used up {:?} bits", res.1);
-                        ver += res.0;
-                        if start_tracking_used {
-                            used += res.1;
-                        }
-                    });
+                    let operands = (0..limit)
+                        .map(|_| {
+                            let res = parse_packet(cursor, start_tracking_used);
+                            // println!("used up {:?} bits", res.1);
+                            ver += res.0;
+                            if start_tracking_used {
+                                used += res.2;
+                            }
+                            res.1
+                        })
+                        .collect::<Vec<usize>>();
                     // println!("Used before after one Operator {:?}", used);
+                    // println!("{:?}", (&operands, usize::from_str_radix(op, 2).unwrap()));
+                    out = operate(&operands, op);
                 }
             }
         }
     }
     // println!("Final {:?}", ver);
-    (ver, used)
+    (ver, out, used)
 }
 
 fn main() {
-    let buf = fs::read_to_string("./test.txt").unwrap();
+    let buf = fs::read_to_string("./input.txt").unwrap();
     let stream = BinaryString::from_hex(buf).unwrap().to_string();
     let buffer = stream.chars().collect::<Vec<char>>();
     let mut cursor = buffer.iter();
