639551bed5d5a8227dd57c60358a734466910236
Aseem Dua <aseem.dua@here.com>
2021-12-17 09:23:39 +0100

    Day 17: Simplify run and purge mem allocs

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,41 +1,26 @@
 use itertools::iproduct;
+use std::cmp::max;
 use std::fs;
 use text_io::scan;
 
-fn run(
-    start: (i32, i32),
-    (x1, x2, y1, y2): (i32, i32, i32, i32),
-    get_positions: bool,
-) -> (Vec<(i32, i32)>, bool) {
-    let (mut pos, mut records, mut overshoot) = ((0, 0), Vec::new(), false);
+fn hits_target(start: (i32, i32), (x1, x2, y1, y2): (i32, i32, i32, i32)) -> bool {
+    let mut pos = (0, 0);
     let (mut x, mut y) = start;
     loop {
         pos = (pos.0 + x, pos.1 + y);
         if pos.0 >= x1 && pos.0 <= x2 && pos.1 >= y1 && pos.1 <= y2 {
-            if get_positions {
-                records.push(pos);
-            }
-            break;
+            return true;
         }
         if pos.1 < y1 {
-            overshoot = true;
-            break;
+            return false;
         }
-        if get_positions {
-            records.push(pos);
-        }
-        if x > 0 {
-            x -= 1;
-        } else if x < 0 {
-            x += 1;
-        }
+        x -= x.signum();
         y -= 1;
     }
-    (records, overshoot)
 }
 
 fn solve_1((x1, x2, y1, y2): (i32, i32, i32, i32)) {
-    let mut max = ((0, 0), 0);
+    let mut max_h = 0;
     /* x velocity is 0 after enough steps.
      * If you want to go highest, take it so that you halt within the target x range
      * abs(y2) is a starting guess for y to minimize iterations
@@ -48,20 +33,17 @@ fn solve_1((x1, x2, y1, y2): (i32, i32, i32, i32)) {
     // empirically found that this is good enough.
     while start_y < y1.abs() {
         let start = (start_x, start_y);
-        let res = run(start, (x1, x2, y1, y2), true);
-        let max_h = res.0.iter().max_by_key(|(_, h)| h).unwrap();
-        if max_h.1 > max.1 && !res.1 {
-            max.0 = start;
-            max.1 = max_h.1;
+        if hits_target(start, (x1, x2, y1, y2)) {
+            max_h = max(max_h, start.1 * (start.1 + 1) / 2)
         }
         start_y += 1;
     }
-    println!("{:?}", max.1);
+    println!("{:?}", max_h);
 }
 
 fn solve_2((x1, x2, y1, y2): (i32, i32, i32, i32)) {
     let count = iproduct![(0..=x2), (y1..=y1.abs())]
-        .filter(|(x, y)| !run((*x, *y), (x1, x2, y1, y2), false).1)
+        .filter(|(x, y)| hits_target((*x, *y), (x1, x2, y1, y2)))
         .count();
     println!("{:?}", count);
 }
